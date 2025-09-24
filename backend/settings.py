630ab78a395fd2f8a31e16a7208c76633e029a33
Shubhansh Sharma <123613015+ShubhanshSharma@users.noreply.github.com>
2025-09-24 21:59:53 +0530

    Update settings.py updation in crsf

--- backend/settings.py
+++ backend/settings.py
@@ -77,10 +77,13 @@ MIDDLEWARE = [
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
+    "http://127.0.0.1:8000",
     "http://certificate-generator-production-6bc2.up.railway.app",
     "https://certificate-generator-production-6bc2.up.railway.app",
     "https://certificate-generator-eosin.vercel.app",
+    "certificate-generator-production-6bc2.up.railway.app"
 ]
+
 
 
 CORS_ALLOW_ALL_ORIGINS = True
